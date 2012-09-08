45bafbae121e6cccc006075b9915bd5a23b53858
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-09-08 00:17:15 -0700

    Error handling improper command line usage

--- pathfinder.rb
+++ pathfinder.rb
@@ -1,14 +1,14 @@
 require 'optparse'
 
-def main(options = {}, filenames)
+def main(options = {}, filenames = [])
   puts options
   puts filenames
 end
 
 if __FILE__ == $PROGRAM_NAME
   options = {}
-  OptionParser.new do |opts|
-    opts.banner = "Usage: pathfinder.rb [options] INFILE1 INFILE2"
+  opts = OptionParser.new do |opts|
+    opts.banner = "Usage: pathfinder.rb [options] INFILE1 [INFILE2 ...]"
     
     opts.on( '-h', '--help', 'Help' ) do
       puts opts
@@ -23,10 +23,17 @@ if __FILE__ == $PROGRAM_NAME
     opts.on('-o FILE', '--output=FILE', "Name of output file") do |o|
       options[:output_file] = o
     end
-  end.parse!
+  end
+  
+  begin
+    opts.parse(ARGV)
+  rescue Exception => e
+    puts e, opts
+    exit
+  end
   
   if ARGV.empty?
-    puts "Oops you need to add at least one file to be analyzed"
+    puts "Oops you need to add at least one file to be analyzed", opts
     exit
   end
   

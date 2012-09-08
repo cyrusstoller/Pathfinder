10499d8931e40277fb34d7af0e97ffb8b62d0df1
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-09-08 13:08:34 -0700

    Allow user to specify number of header rows to ignore

--- pathfinder.rb
+++ pathfinder.rb
@@ -7,17 +7,20 @@ def main(options = {}, filenames = [])
   output_file = File.open(options[:output_file], "w")
   filenames.each do |filename|
     puts "Analyzing '#{filename}'"
-    find_subpaths(filename, output_file , options[:length], options[:verbose])
+    find_subpaths(filename, output_file , options[:length], options[:verbose], options[:header_rows])
   end
   output_file.close
   puts "All jobs complete."
 end
 
-def find_subpaths(filename, output_file, length, verbose = false)
+def find_subpaths(filename, output_file, length, verbose = false, header_rows = 0)
   output_file << filename + "\n"
   paths = {}
+  row_num = 0
 
   CSV.parse(File.read(filename)).each do |row|
+    row_num += 1
+    next if row_num < header_rows + 1
     path = return_paths(row)
     total_path_length = path.size
     if total_path_length >= length
@@ -45,7 +48,7 @@ def turn_paths_to_csv(paths, output_file, verbose = false)
 end
 
 def return_paths(array)
-  r = (array[2..-1]).map do |i|
+  r = (array[1..-1]).map do |i|
     i = i.to_s
     i.strip unless i == "" or i == "\r" or i == "\n"
   end
@@ -57,7 +60,8 @@ def finalize_options(options)
   res = { 
           :length => default_options["path_length"], 
           :output_file => default_options["output_file"],
-          :verbose => default_options["verbose"]
+          :verbose => default_options["verbose"],
+          :header_rows => default_options["header_rows"]
         }
   res.merge(options)
 end
@@ -72,6 +76,10 @@ if __FILE__ == $PROGRAM_NAME
       exit!
     end
     
+    opts.on('-i NUM', '--ignore-rows=NUM', Integer, "Number of header rows to ignore") do |h|
+      options[:header_rows] = h
+    end
+    
     opts.on('-l LENGTH', '--length=LENGTH', Integer, "Length of the subpaths") do |l|
       options[:length] = l
     end
@@ -80,7 +88,7 @@ if __FILE__ == $PROGRAM_NAME
       options[:output_file] = o
     end
     
-    opts.on( '-v', '--[no-]verbose', "Verbose mode" ) do |v|
+    opts.on('-v', '--[no-]verbose', "Verbose mode" ) do |v|
       options[:verbose] = v
     end
   end

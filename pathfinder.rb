bd79cee03622852d595acb1af554021dd226545b
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-09-07 23:15:35 -0700

    Added a length option parameter

--- pathfinder.rb
+++ pathfinder.rb
@@ -9,9 +9,13 @@ if __FILE__ == $PROGRAM_NAME
   OptionParser.new do |opts|
     opts.banner = "Usage: pathfinder.rb [options]"
 
-    opts.on( '-h', '--help', 'Display this screen' ) do
+    opts.on( '-h', '--help', 'Help' ) do
       puts opts
       exit
+    end
+    
+    opts.on('-l LENGTH', '--length=LENGTH', "Length of the subpaths") do |l|
+      options[:length] = l
     end
   end.parse!
   
